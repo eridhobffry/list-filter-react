da669d3344d585a9f090296630f6a260330a46f8
eridhobffry <eridhobffry@github.com>
2020-01-24 12:11:34 +0100

    react functional

--- src/building_blocks/search_bar.js
+++ src/building_blocks/search_bar.js
@@ -1,3 +1,41 @@
 import React from "react";
+import PropsTypes from "prop-types";
 
-export const SearchBar = props => {};
+export const SearchBar = props => {
+  handleFilterTextChange = e => {
+    props.onFilterTextChange(e.target.value);
+  };
+
+  handleInStockChange = e => {
+    props.onInStockChange(e.target.checked);
+  };
+
+  const filterText = props.filterText;
+  const inStockOnly = props.inStockOnly;
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={props.handleFilterTextChange}
+      />
+      <p>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={handleInStockChange}
+        />{" "}
+        Only show products in stock
+      </p>
+    </form>
+  );
+};
+
+SearchBar.propsTypes = {
+  handleFilterTextChange: PropsTypes.func.isRequired,
+  handleInStockChange: PropsTypes.func.isRequired,
+  filterText: PropsTypes.string.isRequired,
+  inStockOnly: PropsTypes.bool.isRequired
+};
